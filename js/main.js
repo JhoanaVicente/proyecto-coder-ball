19950a5a4315a8e4ef2ce96d5909a56804af5464
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-13 12:22:45 +0200

    pparriba

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
-Function Myfunction () {
+let names = ["Telma", "Tatiana", "Irina S", "Jhoana", "Maribel", "Jezabel", "Irina G",
+    "Minerva", "Nadia", "Silvana", "Pamela", "Javi", "Gabriela", "Manuel",
+    "Halima", "Nelly", "Ikram", "Lola"];
 
-    hfg
+let originalNames = ["Telma", "Tatiana", "Irina S", "Jhoana", "Maribel", "Jezabel", "Irina G",
+    "Minerva", "Nadia", "Silvana", "Pamela", "Javi", "Gabriela", "Manuel",
+    "Halima", "Nelly", "Ikram", "Lola"];
+
+
+function generate() {
+    let texto;
+    if (names.length === 0) {
+        texto = "Game Over!"
+    } else {
+        let randomNumber = Math.floor(Math.random() * names.length);
+        texto = names[randomNumber];
+        names.splice(randomNumber, 1);
+    }
+
+    document.getElementById('result').innerHTML =
+        "<div class='alert alert-sucess'>"
+        + texto
+        + "</div>";
+}
+
+function reset() {
+    // copiar de nuevo los nombres originales a la lista que usamos en generate
+}
+
+
+
+function imagesPokemons() {
+    let images = ["Cinthia.png", "Gabriela.png", "Halima.png"];
+    let imagesLength = images.length;
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * imagesLength;
+    randomNumber = Math.floor(randomNumber);
+    let choosenImage = images[randomNumber];
+    pokemons.src = choosenImage;
 }
